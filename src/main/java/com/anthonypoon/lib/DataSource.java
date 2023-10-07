5f7e349eeb3f6338186bc50332b38a9030b79831
Anthony Poon <anthony.poon@emarsys.com>
2023-10-07 12:06:13 +0200

    Make id unqiue in the example

--- src/main/java/com/anthonypoon/lib/DataSource.java
+++ src/main/java/com/anthonypoon/lib/DataSource.java
@@ -9,9 +9,9 @@ public class DataSource {
     public static List<Student> read() {
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, 1, 18, "John Doe"));
-        students.add(new Student(1, 2, 18, "Mary Leung"));
-        students.add(new Student(1, 1, 20, "Peter Pan"));
-        students.add(new Student(1, 1, 22, "Eric Sze"));
+        students.add(new Student(2, 2, 18, "Mary Leung"));
+        students.add(new Student(3, 1, 20, "Peter Pan"));
+        students.add(new Student(4, 1, 22, "Eric Sze"));
         return students;
     }
 }
